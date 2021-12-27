208b311eb20e0fd0f95dee165c81dfe3c65b579e
Arthur-Jr <arthurjr1906@gmail.com>
2021-12-27 15:43:38 -0300

    Updates

---
+++ precache-manifest.888a47432adc73d46e72f43184dd24be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1582086992a1180fe440dd01a370709b",
+    "url": "/starwars-planet-finder/index.html"
+  },
+  {
+    "revision": "8f9a544ac0566f9011a8",
+    "url": "/starwars-planet-finder/static/css/main.95b95f30.chunk.css"
+  },
+  {
+    "revision": "6c7a1466a083f2ebe932",
+    "url": "/starwars-planet-finder/static/js/2.97282e9a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/starwars-planet-finder/static/js/2.97282e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9a544ac0566f9011a8",
+    "url": "/starwars-planet-finder/static/js/main.6ad27557.chunk.js"
+  },
+  {
+    "revision": "06408738a4e85cc3b878",
+    "url": "/starwars-planet-finder/static/js/runtime-main.68e0b9d6.js"
+  },
+  {
+    "revision": "922ab2ca483b31798295c87f958654a8",
+    "url": "/starwars-planet-finder/static/media/background-img.922ab2ca.jpg"
+  },
+  {
+    "revision": "f25d8a38d705816c268592529e434b55",
+    "url": "/starwars-planet-finder/static/media/gitHubIcon.f25d8a38.svg"
+  },
+  {
+    "revision": "652200391c1a0edbd3dcd8fdf3e39a6a",
+    "url": "/starwars-planet-finder/static/media/linkedinIcon.65220039.svg"
+  }
+]);
